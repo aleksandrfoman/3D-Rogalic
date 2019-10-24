fc93820d3a98544eb4720e69fd44c053bcce2957
Foman <aleksandrfoman@gmail.com>
2019-10-24 22:28:43 +0300

    Движение камерой сделанно

---
+++ 3D_Rogalic/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes 
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVer = 9.0f;
+    public float minVer = -45f;
+    public float maxVer = 45f;
+
+    private float _rotationX = 0f;
+
+    private void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0,Input.GetAxis("Mouse X")*sensitivityHor,0);  //Это поворот в горизонтальной плоскости 
+        } 
+        else if (axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+            _rotationX = Mathf.Clamp(_rotationX, minVer, maxVer);
+            float rotationY = transform.localEulerAngles.y;
+            
+            transform.localEulerAngles = new Vector3(_rotationX,rotationY,0);
+            //Это поворот в горизонтальной плоскости
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+            _rotationX = Mathf.Clamp(_rotationX, minVer, maxVer);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+            
+            transform.localEulerAngles=new Vector3(_rotationX,rotationY,0);
+            
+            //Это комбиноторный поворот
+        }
+    }
+}
--- 3D_Rogalic/Assets/Scripts/Spin.cs
+++ 3D_Rogalic/Assets/Scripts/Spin.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 3f;
+    
     void Update()
     {
+        transform.Rotate(0,speed,0);
         
     }
 }
