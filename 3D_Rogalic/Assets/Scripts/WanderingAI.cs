5fb9703b93ccf98d667d5920243bb162d415b097
Aleksandr <aleksandrfoman@gmail.com>
2019-11-18 21:08:05 +0200

    Повреждение игрока+shoting and t.d

--- 3D_Rogalic/Assets/Scripts/WanderingAI.cs
+++ 3D_Rogalic/Assets/Scripts/WanderingAI.cs
@@ -7,6 +7,8 @@ public class WanderingAI : MonoBehaviour
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     private bool _alive;
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
 
     private void Start()
     {
@@ -22,8 +24,17 @@ public class WanderingAI : MonoBehaviour
         RaycastHit hit;
         if (Physics.SphereCast(ray, 0.75f, out hit))
         {
-            Debug.DrawRay(ray.origin,hit.point,Color.black);
-            if (hit.distance < obstacleRange)
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.GetComponent<PlayerCharcter>())
+            {
+                if (_fireball == null)
+                {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+            }
+            else if (hit.distance < obstacleRange)
             {
                 float angle = Random.Range(-110, 110);
                 transform.Rotate(0, angle, 0);
