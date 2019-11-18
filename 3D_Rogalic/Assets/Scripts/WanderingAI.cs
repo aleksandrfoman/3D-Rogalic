dcc4666db4661ab78168e7079deef16cc03b6d8a
Aleksandr <aleksandrfoman@gmail.com>
2019-11-18 19:50:43 +0200

    Слежение за состоянием персонажа

---
+++ 3D_Rogalic/Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    private bool _alive;
+
+    private void Start()
+    {
+        _alive = true;
+    }
+    private void Update()
+    {
+        if (_alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            Debug.DrawRay(ray.origin,hit.point,Color.black);
+            if (hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
+    }
+}
